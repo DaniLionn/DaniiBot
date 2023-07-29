ebfef9dc0ee1fb4d01724ac5bd5af2218b2a887c
DaniLionn <107291896+DaniLionn@users.noreply.github.com>
2023-07-28 20:33:44 -0600

    update

--- commands/convert-files.js
+++ commands/convert-files.js
@@ -6,6 +6,9 @@ const ffmpeg = require('fluent-ffmpeg');
 ffmpeg.setFfmpegPath(ffmpegStatic);
 const https = require('https'); // or 'https' for https:// URLs
 const fs = require('fs-extra');
+const { exec } = require("child_process");
+
+
 module.exports = {
         data: new SlashCommandBuilder()
             .setName('convert-file')
@@ -18,7 +21,9 @@ module.exports = {
                 option.setName('extension')
                 .setDescription('the file you want to convert to')
                 .setRequired(true)
+                
             ),
+            
         async execute(interaction) {
             
             await interaction.deferReply()
@@ -27,11 +32,16 @@ module.exports = {
             
             const extension = interaction.options.getString("extension")
             
+            const newName = undefined
+
             const name = attachment.name
             const url = attachment.url
             const proxyURL = attachment.proxyURL
             
             let path
+            let path2
+
+            console.log(name, name.split(".")[0])
             
             console.log("downloading file")
             
@@ -40,24 +50,63 @@ module.exports = {
                     response.pipe(file);
                     
                     // after download completed close filestream
-                    file.on("finish", () => {
-                        
-
-
-                        console.log("Download Completed", file.path);
-
-                        fs.move(file.path, `../tempConvert/${file.path}`, err => {
-                            if (err) return console.error(err)
-                            console.log('success!')
-                          })
+                    file.on("finish",  async () => {
                         
                         path = file.path
-                        file.close(cb); // close() is async, call cb after close completes.
+
+                        if (extension.includes(".")) {
+
+                            console.log(".")
+                            path2 = `${name.split(".")[0]}${extension}`
+                        
+
+                        } else {
+                            path2 = `${name.split(".")[0]}.${extension}`
+                        }
+                       
+                        console.log(path2)
+                        file.close(); // close() is async, call cb after close completes.
+
+                        
+
+                        exec(`ffmpeg -i ${path} ${path2}`, async (error, stdout, stderr) => {
+                            if (error) {
+                                console.log(`error: ${error.message}`);
+                                await interaction.channel.send("An error occured! Try checking what you're trying to convert to. Make sure it's a similar file type (for example, you can do mp3 to wav but not mp3 to txt)")
+                                return;
+                            }
+                            if (stderr) {
+                                console.log(`stderr: ${stderr}`);
+                                return;
+                            }
+                            console.log(`stdout: ${stdout}`);
+                            return
+                        
+                        }).on("close", async (code) => {
+                            console.log("done")
+                            console.log(code)
+
+
+                            await interaction.deleteReply()
+
+                            if (code === 0) {
+                            await interaction.channel.send({
+                                content: `<@${interaction.user.id}> Your file was converted!`,
+                                files: [path2]
+                            })
+    
+                            setTimeout(() => {
+                                fs.unlink(path);
+                            fs.unlink(path2);
+                            }, 100)
+                    }});;
+
+                       
                     });
                 })
                 .on('error', function (err) { // Handle errors
                     fs.unlink(dest); // Delete the file async. (But we don't check the result)
-                    if (cb) cb(err.message);
+                    
                 });
             
             
