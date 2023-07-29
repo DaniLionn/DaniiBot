a266209b4737144c5c0e462ab9b2a412eb4bd474
DaniLionn <107291896+DaniLionn@users.noreply.github.com>
2023-07-28 21:10:30 -0600

    kl

--- commands/convert-files.js
+++ commands/convert-files.js
@@ -6,8 +6,9 @@ const ffmpeg = require('fluent-ffmpeg');
 ffmpeg.setFfmpegPath(ffmpegStatic);
 const https = require('https'); // or 'https' for https:// URLs
 const fs = require('fs-extra');
-const { exec } = require("child_process");
-
+const {
+    exec
+} = require("child_process");
 
 module.exports = {
         data: new SlashCommandBuilder()
@@ -23,94 +24,96 @@ module.exports = {
                 .setRequired(true)
                 
             ),
-            
+        
         async execute(interaction) {
             
-            await interaction.deferReply()
+            await interaction.deferReply("Starting...", {
+                ephemeral: true
+            })
             
-            const attachment = interaction.options.getAttachment("file")
-            
-            const extension = interaction.options.getString("extension")
-            
-            const newName = undefined
-
-            const name = attachment.name
-            const url = attachment.url
-            const proxyURL = attachment.proxyURL
-            
-            let path
-            let path2
-
-            console.log(name, name.split(".")[0])
-            
-            console.log("downloading file")
-            
-            const file = fs.createWriteStream(name);
-            const request = https.get(url, function (response) {
-                    response.pipe(file);
-                    
-                    // after download completed close filestream
-                    file.on("finish",  async () => {
+            setTimeout(async () => {
+                
+                const attachment = interaction.options.getAttachment("file")
+                
+                const extension = interaction.options.getString("extension")
+                
+                const newName = undefined
+                
+                const name = attachment.name
+                const url = attachment.url
+                const proxyURL = attachment.proxyURL
+                
+                let path
+                let path2
+                
+                console.log(name, name.split(".")[0])
+                
+                console.log("downloading file")
+                
+                await interaction.editReply("Downloading...")
+                
+                const file = fs.createWriteStream(name);
+                const request = https.get(url, function (response) {
+                        response.pipe(file);
                         
-                        path = file.path
-
-                        if (extension.includes(".")) {
-
-                            console.log(".")
-                            path2 = `${name.split(".")[0]}${extension}`
-                        
-
-                        } else {
-                            path2 = `${name.split(".")[0]}.${extension}`
-                        }
-                       
-                        console.log(path2)
-                        file.close(); // close() is async, call cb after close completes.
-
-                        
-
-                        exec(`ffmpeg -i ${path} ${path2}`, async (error, stdout, stderr) => {
-                            if (error) {
-                                console.log(`error: ${error.message}`);
-                                await interaction.channel.send("An error occured! Try checking what you're trying to convert to. Make sure it's a similar file type (for example, you can do mp3 to wav but not mp3 to txt)")
-                                return;
+                        // after download completed close filestream
+                        file.on("finish", async () => {
+                            
+                            path = file.path
+                            
+                            if (extension.includes(".")) {
+                                
+                                console.log(".")
+                                path2 = `${name.split(".")[0]}${extension}`
+                                
+                            } else {
+                                path2 = `${name.split(".")[0]}.${extension}`
                             }
-                            if (stderr) {
-                                console.log(`stderr: ${stderr}`);
-                                return;
-                            }
-                            console.log(`stdout: ${stdout}`);
-                            return
+                            
+                            console.log(path2)
+                            file.close(); // close() is async, call cb after close completes.
+                            
+                            await interaction.editReply("Converting...")
+                            exec(`ffmpeg -i ${path} ${path2}`, async (error, stdout, stderr) => {
+                                    if (error) {
+                                        console.log(`error: ${error.message}`);
+                                        await interaction.channel.send("An error occured! Try checking what you're trying to convert to. Make sure it's a similar file type (for example, you can do mp3 to wav but not mp3 to txt)")
+                                        return;
+                                    }
+                                    if (stderr) {
+                                        console.log(`stderr: ${stderr}`);
+                                        return;
+                                    }
+                                    console.log(`stdout: ${stdout}`);
+                                    return
+                                    
+                                })
+                                .on("close", async (code) => {
+                                    console.log("done")
+                                    console.log(code)
+                                    
+                                    if (code === 0) {
+                                        await interaction.channel.send({
+                                            content: `<@${interaction.user.id}> Your file was converted!`,
+                                            files: [path2]
+                                        })
+                                        await interaction.deleteReply()
+                                        
+                                        setTimeout(() => {
+                                            fs.unlink(path);
+                                            fs.unlink(path2);
+                                        }, 100)
+                                    }
+                                });;
+                            
+                        });
+                    })
+                    .on('error', function (err) { // Handle errors
+                        fs.unlink(dest); // Delete the file async. (But we don't check the result)
                         
-                        }).on("close", async (code) => {
-                            console.log("done")
-                            console.log(code)
-
-
-                            await interaction.deleteReply()
-
-                            if (code === 0) {
-                            await interaction.channel.send({
-                                content: `<@${interaction.user.id}> Your file was converted!`,
-                                files: [path2]
-                            })
-    
-                            setTimeout(() => {
-                                fs.unlink(path);
-                            fs.unlink(path2);
-                            }, 100)
-                    }});;
-
-                       
                     });
-                })
-                .on('error', function (err) { // Handle errors
-                    fs.unlink(dest); // Delete the file async. (But we don't check the result)
-                    
-                });
-            
-            
-    
+                
+            })
         }
         
     },
