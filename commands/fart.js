35e30a3e3d6e1650f57fab280070835df7163b79
DaniLionn <107291896+DaniLionn@users.noreply.github.com>
2023-07-30 22:39:52 -0600

    Update fart.js

--- commands/fart.js
+++ commands/fart.js
@@ -1,6 +1,6 @@
 const { SlashCommandBuilder } = require('discord.js');
 
-const farts = ["../assets/farts/fart1.mp3", "../assets/farts/fart2.mp3", "../assets/farts/fart3.mp3", "../assets/farts/fart4.mp3", "../assets/farts/fart5.mp3", "../assets/farts/fart6.mp3", "../assets/farts/fart7.mp3", "../assets/farts/fart8.mp3", "../assets/farts/fart9.mp3", "../assets/farts/fart10.mp3", "../assets/farts/fart11.mp3", "../assets/farts/fart12.mp3", "../assets/farts/fart13.mp3", "../assets/farts/fart14.mp3", "../assets/farts/fart15.mp3"] 
+const farts = ["assets/farts/fart1.mp3", "assets/farts/fart2.mp3", "assets/farts/fart3.mp3", "assets/farts/fart4.mp3", "assets/farts/fart5.mp3", "assets/farts/fart6.mp3", "assets/farts/fart7.mp3", "assets/farts/fart8.mp3", "assets/farts/fart9.mp3", "assets/farts/fart10.mp3", "assets/farts/fart11.mp3", "assets/farts/fart12.mp3", "assets/farts/fart13.mp3", "assets/farts/fart14.mp3", "assets/farts/fart15.mp3"] 
 
 const wait = require('node:timers/promises').setTimeout;
 
