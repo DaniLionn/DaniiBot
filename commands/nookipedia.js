f965470760af9dafba87f1935e22dc4ef7f5ec6c
DaniLionn <107291896+DaniLionn@users.noreply.github.com>
2023-08-03 22:35:28 -0600

    Update nookipedia.js

--- commands/nookipedia.js
+++ commands/nookipedia.js
@@ -753,7 +753,7 @@ module.exports = {
                             
                             if (FinalJSON["has_fake"] === true) {
                                 
-                                const canvas = createCanvas(400, 500);
+                                const canvas = createCanvas(500, 500);
                                 const context = canvas.getContext('2d');
                                 
                                 download(FinalJSON["real_info"]["texture_url"], "real.png")
@@ -767,11 +767,11 @@ module.exports = {
                                 const fakeImage =await readFile("./fake.png")
                                 const real = new Image();
                                 real.src = realImage
-                                context.drawImage(real, 0, 0, 200, canvas.height);
+                                context.drawImage(real, 0, 0, 250, canvas.height);
                                 
                                 const fake = new Image();
                                 fake.src = fakeImage;
-                                context.drawImage(fake, 200, 0, 200, canvas.height);
+                                context.drawImage(fake, 250, 0, 250, canvas.height);
                                 
                                 context.font = "32px Courier New";
                                 context.fillStyle = '#ffffff';
@@ -784,8 +784,8 @@ module.exports = {
                                 context.fillStyle = '#ffffff';
                                 context.strokeStyle = 'black';
                                 context.lineWidth = 6;
-                                context.strokeText("FAKE", 400, (500 - 32));
-                                context.fillText("FAKE", 400, (500 - 32));
+                                context.strokeText("FAKE", 300, (500 - 32));
+                                context.fillText("FAKE", 300, (500 - 32));
                                 
                                 const attachment = new AttachmentBuilder(canvas.toBuffer('image/png'), {
                                     name: 'artguide.png'
