29558f95574d6d56858c55b35b50e800c8367b86
DaniLionn <107291896+DaniLionn@users.noreply.github.com>
2023-09-21 13:08:12 -0600

    Create invite.js

---
+++ commands/invite.js
@@ -0,0 +1,27 @@
+const { SlashCommandBuilder } = require('discord.js');
+const https = require("https")
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('invite')
+		.setDescription("creates an invite to generic vibe"),
+
+	async execute(interaction) {
+		const channel = interaction.channel.id
+
+		https.get(`https://daniibot.dani-lionn.repl.co/postInvite?channelID=${channel}&placeID=11138886508&message=<@1130288851521392750>%20wanna%20play?`)
+		
+	},
+};
+const fs = require('node:fs');
+process.on('unhandledRejection', error =>
+{
+	console.log("Error detected! Saving to error log...")
+	let s = new Date().toLocaleString();
+	const read = fs.readFileSync('./ErrorLog.txt', 'utf8', err => {if (err) {console.log(err)}}) 
+	const data = `${read}\n${s}: ${error}`
+	//console.log(data)
+	fs.writeFileSync('./ErrorLog.txt', data, err =>{ if (err) {console.error(err);}
+		// file written successfully
+		console.log("Successfully wrote error!")
+	});
+});
