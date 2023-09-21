e747354b50ac0efa8050e4b8d97cb884ffd4bf4a
DaniLionn <107291896+DaniLionn@users.noreply.github.com>
2023-09-21 13:21:31 -0600

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -7,9 +7,10 @@ module.exports = {
 
 	async execute(interaction) {
 		const channel = interaction.channel.id
+		const user = interaction.user.username
 		await interaction.deferReply({ephemeral: true})
 
-		https.get(`https://daniibot.dani-lionn.repl.co/postInvite?channelID=${channel}&placeID=11138886508&message=<@1130288851521392750>%20wanna%20play?`)
+		https.get(`https://daniibot.dani-lionn.repl.co/postInvite?channelID=${channel}&placeID=11138886508&name=Invite%20sent%20by%20${user}&message=<@1130288851521392750>%20wanna%20play?`)
 		await interaction.deleteReply()
 	},
 };
