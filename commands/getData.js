4a3b1f9d36d348a4abb5cdf524d66aa62c06f7e8
DaniLionn <107291896+DaniLionn@users.noreply.github.com>
2023-10-04 16:09:31 -0600

    Update getData.js

--- commands/getData.js
+++ commands/getData.js
@@ -1,12 +1,39 @@
 const { SlashCommandBuilder } = require('discord.js');
-
+var fs = require("fs");
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('get-generic-vibe-data')
-		.setDescription("gets player data from generic vibe"),
+		.setDescription("gets player data from generic vibe")
+		.addStringOption(option =>
+			option.setName('UserId')
+				.setDescription('the userid of the player whose data you want')
+				.setRequired(true)),
+		
 	async execute(interaction) {
+		
 
-	},
+		https.get(`https://daniibot.dani-lionn.repl.co/getData?UserId=${interaction.options.getString('UserId')}`, options, (response) => {
+
+		var result = ''
+		response.on('data', function(chunk) {
+			result += chunk;
+		});
+	
+		response.on('end', async function() {
+			FinalJSON = JSON.parse(result);
+	
+			var createStream = fs.createWriteStream("data.json");
+			createStream.end();
+			writeStream.write(FinalJSON);
+
+			writeStream.end();
+
+			await interaction.channel.send({
+				files: ['./data.json']
+			});
+			
+				})
+	})}
 };
 const fs = require('node:fs');
 process.on('unhandledRejection', error =>
